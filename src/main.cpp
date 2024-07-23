e8367695da873e64424ffb4e6fa05ab077effa63
TheGetGoodGit2211 <jaymideansheldon@gmail.com>
2024-07-23 18:03:35 +0100

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,20 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+  auto window = sf::RenderWindow{{1920u, 1080u}, "CMake SFML Project"};
+  window.setFramerateLimit(144);
 
-    while (window.isOpen())
+  while (window.isOpen())
+  {
+    for (auto event = sf::Event{}; window.pollEvent(event);)
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
     }
+
+    window.clear();
+    window.display();
+  }
 }
